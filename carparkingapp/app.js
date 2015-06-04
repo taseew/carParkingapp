353205e36e0473f1eb0f1f22ae9b31c9a29972ca
Satish Viswanathan <satish@INSVISWAN1.local>
2015-06-04 19:08:44 +0530

    added page to create a new slot

--- carparkingapp/app.js
+++ carparkingapp/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var slots = require('./routes/slots');
+var addSlot = require('./routes/addSlot');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/slots', slots);
+app.use('/slot/new', addSlot);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
