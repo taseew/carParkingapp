aeadf394e1d9f22434a24d4d0a789eef2d1b89f5
ponnulingam <ponnulingam.r@gmail.com>
2015-06-10 17:58:02 +0530

    [Ponnu/Arvind] Added link for new slot

--- carparkingapp/app.js
+++ carparkingapp/app.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var slots = require('./routes/slots');
-var addSlot = require('./routes/addSlot');
 
 var app = express();
 
@@ -27,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/slots', slots);
-app.use('/slot/new', addSlot);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- carparkingapp/routes/slots.js
+++ carparkingapp/routes/slots.js
@@ -3,10 +3,19 @@ var router = express.Router();
 var fs = require('fs');
 
 /* GET slots listing. */
-router.get('/', function(req, res, next) {
-	var data = JSON.parse(fs.readFileSync("./data/slotnumbers.json"));
-	console.log("Slots file", data.slotNumbers);
-    res.render('slots', { title: 'List of Slots', slots: data.slotNumbers});
+router.get('/', function (req, res, next) {
+  var data = JSON.parse(fs.readFileSync("./data/slotnumbers.json"));
+  console.log("Slots file", data.slotNumbers);
+  res.render('slots', { title: 'List of Slots', slots: data.slotNumbers});
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('newSlot', { title: 'Add New Slot'});
+});
+
+router.post('/new', function (req, res, next) {
+  console.log("Create slot");
+  res.redirect('/slots');
 });
 
 module.exports = router;
